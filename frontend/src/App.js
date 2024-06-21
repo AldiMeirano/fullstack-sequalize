2671b0f584a764c183afcd2835e419cdbba008eb
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2024-06-21 18:45:12 +0100

    feat: setup react core (#1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <h1 className="bg-red-400">Hello guys</h1>;
-}
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
