d6c821f8f98ff5a33683160fbdaddbe41dca4a11
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2024-06-23 14:49:30 +0100

    feat: landing pages (#2)

---
+++ frontend/src/components/LandingPage/Corusel.jsx
@@ -0,0 +1,110 @@
+/* eslint-disable jsx-a11y/alt-text */
+const Carousel = () => {
+  return (
+    <div className="">
+      <div className="carousel w-full ">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Nugi/Cashback/Main%20Banner%20Cashnback%20Jakarta.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Nugi/Diskon/Main%20Banner%20Mid%20Year%20Sale%20eCom.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Nugi/Frenzy/Main%20Banner%20Nonfiction%20Frenzy%2020.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Nugi/Umum/Banner%20Paus%20Francis.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Banner_Main/Main%20New/lp_dropship.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Banner_Main/Main%20New/voucher_periplus.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://www.periplus.com/image/other/Banner_Main/Main%20New/lp_pec_kecil.jpg"
+            className="w-full"
+          />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// const ads = require('')
+export default Carousel;
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,21 @@
-const LandingPage = () => { 
-    return( 
-    <div className="bg-green-900">Hello guys</div>
-    )
-}
+import AllProduct from "../components/LandingPage/AllProduct";
+import BookHoot from "../components/LandingPage/BookHo";
+import Category from "../components/LandingPage/Category";
+import Carousel from "../components/LandingPage/Corusel";
+import Footer from "../components/LandingPage/Footer";
+import UpCommingBook from "../components/LandingPage/UpCommingBook";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Carousel />
+      <Category />
+      <UpCommingBook />
+      <BookHoot />
+      <AllProduct />
+      <Footer />
+    </div>
+  );
+};
 
 export default LandingPage;
